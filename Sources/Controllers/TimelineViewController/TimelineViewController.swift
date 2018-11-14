d166f312756c52b9d4d6386f1adc61f5bb6fea60
Tim Davis <davistim@me.com>
2018-11-14 10:45:48 -0800

    Reduced access level of view outlets.

--- Sources/Controllers/TimelineViewController/TimelineViewController.swift
+++ Sources/Controllers/TimelineViewController/TimelineViewController.swift
@@ -11,16 +11,16 @@ import MacGestures
 class TimelineViewController: NSViewController, GestureResponder, SelectionHandler, NSCollectionViewDelegateFlowLayout {
     static let storyboard = "Timeline"
 
-    @IBOutlet weak var timelineBackgroundView: NSView!
-    @IBOutlet weak var timelineCollectionView: FlippedCollectionView!
-    @IBOutlet weak var timelineClipView: NSClipView!
-    @IBOutlet weak var timelineScrollView: NSScrollView!
-    @IBOutlet weak var timelineBaseView: NSView!
-    @IBOutlet weak var decadeCollectionView: NSCollectionView!
-    @IBOutlet weak var decadeClipView: NSClipView!
-    @IBOutlet weak var decadeScrollView: NSScrollView!
-    @IBOutlet weak var timelineIndicatorView: NSView!
-    @IBOutlet weak var timelineBottomConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var timelineBackgroundView: NSView!
+    @IBOutlet private weak var timelineCollectionView: FlippedCollectionView!
+    @IBOutlet private weak var timelineClipView: NSClipView!
+    @IBOutlet private weak var timelineScrollView: NSScrollView!
+    @IBOutlet private weak var timelineBaseView: NSView!
+    @IBOutlet private weak var decadeCollectionView: NSCollectionView!
+    @IBOutlet private weak var decadeClipView: NSClipView!
+    @IBOutlet private weak var decadeScrollView: NSScrollView!
+    @IBOutlet private weak var timelineIndicatorView: NSView!
+    @IBOutlet private weak var timelineBottomConstraint: NSLayoutConstraint!
 
     var gestureManager: GestureManager!
     private(set) var currentDate = Constants.initialDate
